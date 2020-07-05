483184f5a9dc4263c8c0595e8fe25f9263e96d68
Iggor <iggorcesar.cardoso@gmail.com>
2020-07-05 12:45:23 -0300

    implement sliding window

--- LeetCode/slidingWindow/index.py
+++ LeetCode/slidingWindow/index.py
@@ -1,4 +1,19 @@
-def sliding_window():
+def slidingWindow(array,k):
 
-    pass
+    tamanho = len(array)
+    if k <=0 and k>tamanho:
+        return 0
 
+    max_sum = sum(array[:k])
+    sum_atual = max_sum
+    
+    for i in range(0,len(array)-k):
+        sum_atual = sum_atual - array[i] + array[k+i]
+        max_sum = max(max_sum,sum_atual)
+
+    return max_sum
+
+if __name__=="__main__":
+    list_num = [1,-40,23,10,23,50,-33,28]
+    window = 3
+    print(slidingWindow(list_num,window))
