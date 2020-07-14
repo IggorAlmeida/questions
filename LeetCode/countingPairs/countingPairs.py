e4f17597119b238b1dd578dcc1ad8f4b862a065c
Iggor <iggorcesar.cardoso@gmail.com>
2020-07-13 22:11:13 -0300

    counting pairs

---
+++ LeetCode/countingPairs/countingPairs.py
@@ -0,0 +1,26 @@
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    cont = 0
+    pair = set()
+    for num in ar:
+        if num in pair:
+            pair.remove(num)
+            cont+=1
+        else:
+            pair.add(num)
+    
+    return cont 
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
